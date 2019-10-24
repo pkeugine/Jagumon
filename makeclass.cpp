3fa2d40dbc5b0cc95eb47e5677ba17bac4c58ded
BeomYoung <50603295+BeomYoung@users.noreply.github.com>
2019-10-24 14:24:08 +0900

    Jagumon Class

---
+++ makeclass.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+class Jagumon{
+private:
+    int power;
+    int defence;
+    int speed;
+    int stamina;
+
+public:
+    Jagumon(int power, int defence, int speed, int stamina){
+        this->power = power;
+        this->defence = defence;
+        this->speed = speed;
+        this->stamina = stamina;
+    }
+};
+
+int main(){
+}
