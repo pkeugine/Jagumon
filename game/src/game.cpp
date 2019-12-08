f9c8a8a52584d5a312b6a334bfe6237cbfea87e9
Eugine Park <pkeugine@gmail.com>
2019-12-08 13:37:05 +0900

    add dead situations

--- game/src/game.cpp
+++ game/src/game.cpp
@@ -612,6 +612,19 @@ void actingBySpeed(Jagumon &player, Jagumon &opponent, char choice) {
         }  
       }
       else if(opponent.showStamina() == 0) {
+        story_position = 0;
+          //werase(main_window);
+          while(story_position < battle_text[4].length()) {
+            wattron(main_window, A_BOLD);
+            mvwaddch(main_window, 20, 5 + story_position, battle_text[4][story_position]);
+            wattroff(main_window, A_BOLD);
+            story_position++;
+            usleep(10000);
+            wrefresh(main_window);
+          }
+          sleep(2);
+          mvwhline(main_window, 20, 1, ' ', screen_area.width() - 2);
+        }
       }
     }
     else if(player.showSpeed() < opponent.showSpeed()) {
@@ -645,19 +658,36 @@ void actingBySpeed(Jagumon &player, Jagumon &opponent, char choice) {
         sleep(2);
         mvwhline(main_window, 20, 1, ' ', screen_area.width() - 2);
       }
-      Attack(player, opponent);
-      story_position = 0;
-      //werase(main_window);
-      while(story_position < battle_text[0].length()) {
-        wattron(main_window, A_BOLD);
-        mvwaddch(main_window, 20, 5 + story_position, battle_text[0][story_position]);
-        wattroff(main_window, A_BOLD);
-        story_position++;
-        usleep(10000);
-        wrefresh(main_window);
+      if(player.showStamina() > 0) {
+        Attack(player, opponent);
+        story_position = 0;
+        //werase(main_window);
+        while(story_position < battle_text[0].length()) {
+          wattron(main_window, A_BOLD);
+          mvwaddch(main_window, 20, 5 + story_position, battle_text[0][story_position]);
+          wattroff(main_window, A_BOLD);
+          story_position++;
+          usleep(10000);
+          wrefresh(main_window);
+        }
+        sleep(2);
+        mvwhline(main_window, 20, 1, ' ', screen_area.width() - 2);
       }
-      sleep(2);
-      mvwhline(main_window, 20, 1, ' ', screen_area.width() - 2);
+      else if(player.showStamina() == 0) {
+        story_position = 0;
+          //werase(main_window);
+          while(story_position < battle_text[5].length()) {
+            wattron(main_window, A_BOLD);
+            mvwaddch(main_window, 20, 5 + story_position, battle_text[5][story_position]);
+            wattroff(main_window, A_BOLD);
+            story_position++;
+            usleep(10000);
+            wrefresh(main_window);
+          }
+          sleep(2);
+          mvwhline(main_window, 20, 1, ' ', screen_area.width() - 2);
+        }
+      }
     }
   }
   else if(choice == 'd') {
